021bed52d4ccc6945f08c692ee22935c485aae7a
fru1tworld <fruitworld.planet@gmail.com>
2025-02-10 20:18:46 +0900

    fix: AuthController SetCookie 설정 추가 및 RequestHeader로 처리될 수 있게 변경

--- fru1t-board/src/main/java/fru1t/fru1tboard/auth/AuthController.java
+++ fru1t-board/src/main/java/fru1t/fru1tboard/auth/AuthController.java
@@ -3,28 +3,39 @@ package fru1t.fru1tboard.auth;
 import fru1t.fru1tboard.auth.dto.LoginRequest;
 import fru1t.fru1tboard.auth.dto.RefreshRequest;
 import fru1t.fru1tboard.auth.dto.TokenResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1")
 public class AuthController {
     private final AuthService authService;
 
-    @PostMapping("/login")
-    public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest request) {
-        return ResponseEntity.ok(authService.login(request));
+    @PostMapping("/v1/login")
+    public ResponseEntity<Void> login(@RequestBody LoginRequest request, HttpServletResponse response) {
+        TokenResponse tokenResponse = authService.login(request);
+        ResponseCookie refreshTokenCookie = ResponseCookie.from("refresh_token", tokenResponse.getRefreshToken())
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("Strict")
+                .path("/")
+                .maxAge(60 * 60 * 24 * 7)
+                .build();
+
+        response.addHeader("Set-Cookie", refreshTokenCookie.toString());
+        return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/logout")
-    public ResponseEntity<Void> logout(@RequestBody String token) {
-        authService.logout(token);
+    @PostMapping("/v1/logout")
+    public ResponseEntity<Void> logout(@RequestHeader("Authorization") String token) {
+        authService.logout(token.replace("Bearer ", ""));
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("/refresh")
+    @PostMapping("/v1/refresh")
     public ResponseEntity<TokenResponse> refreshToken(@RequestBody RefreshRequest request) {
         return ResponseEntity.ok(authService.refreshToken(request));
     }
